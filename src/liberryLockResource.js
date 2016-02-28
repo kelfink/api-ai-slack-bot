145ce91ebce9b42f8afabcb3b2cf6d8a2264d500
Jason Gibbs <Jason Gibbs>
2016-02-28 13:02:14 -0800

    Testing pretty names

--- src/liberryLockResource.js
+++ src/liberryLockResource.js
@@ -3,8 +3,6 @@
 // ============================
 
 var lockResource = function (bot, message, params) {
-	const utils = require('./utils.js');
-
 	var pg = require('pg');
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 			client.query("UPDATE resources set checkedout_to_id = $2 where checkedout_to_id IS NULL AND name = $1", [params.resource_name, message.user], function(err, result) {
@@ -26,16 +24,12 @@ var lockResource = function (bot, message, params) {
 					      bot.reply(message, "I couldn't find resource " + params.resource_name);
 					     return;
 			            } else {
-					        utils.usersList( function(userMap) {
-								bot.reply(message, "Resource " + params.resource_name + " is already checked out by " + userMap[result.rows[0].checkedout_to_id].name);
-							});
+				          bot.reply(message, "Resource " + params.resource_name + " is already locked by " + result.rows[0].checkedout_to_id);
 			            }
 					  }
 					});	
 				  } else {
-					utils.usersList( function(userMap) {
-						bot.reply(message, "Resource " + params.resource_name + " is already checked out to you, " + userMap[message.user].name);
-					});
+					bot.reply(message, "Resource " + params.resource_name + " is checked out to you, " + message.user );
 				  }
 				}
               }
