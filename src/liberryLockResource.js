9c5318189d43ac5d0f95e864282bfd05b080212b
Jason Gibbs <Jason Gibbs>
2016-02-28 14:33:32 -0800

    rolling back issue with lockresource

--- src/liberryLockResource.js
+++ src/liberryLockResource.js
@@ -26,23 +26,16 @@ var lockResource = function (bot, message, params) {
 					      bot.reply(message, "I couldn't find resource " + params.resource_name);
 					     return;
 			            } else {
-                          if (message.user == result.rows[0].checkedout_to_id) {
-				            bot.reply(message, "You already have Resource " + params.resource_name + " checked out.");
-                          } else {
-				            bot.reply(message, "Resource " + params.resource_name + " is already locked by " + result.rows[0].checkedout_to_id);
-                          }
-			            }
-					    utils.usersList( function(userMap) {
-						  bot.reply(message, "Resource " + params.resource_name + " is already checked out by " + userMap[result.rows[0].checkedout_to_id].name);
+					        utils.usersList( function(userMap) {
+								bot.reply(message, "Resource " + params.resource_name + " is already checked out by " + userMap[result.rows[0].checkedout_to_id].name);
 							});
-						return;
 						}
-					  });
+					  }
+					});	
 				  } else {
 					utils.usersList( function(userMap) {
 						bot.reply(message, "Resource " + params.resource_name + " is now checked out to you, " + userMap[message.user].name);
 					});
-					return;
 				  }
 				}
               }
