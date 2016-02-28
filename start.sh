6ae02e8136b22a89d207dcfa8738d94148830be1
Kevin Fries <kelfink@yahoo.com>
2016-02-27 17:58:35 -0800

    Remove obsolete keys reference

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-. keys.sh
-
 node src/index.js --accesskey="$accesskey" \
                   --subscriptionkey="$subscriptionkey" \
                   --slackkey="$slackkey" \
