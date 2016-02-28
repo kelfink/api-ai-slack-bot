7d7397f8b09a0569a8a7cb74dcd7dff5bfbc1cf2
Kevin Fries <kelfink@yahoo.com>
2016-02-28 11:41:30 -0800

    Fix lock resource command

--- src/liberryLockResource.js
+++ src/liberryLockResource.js
@@ -16,33 +16,35 @@ var lockResource = function (bot, message, params) {
 			client.query("UPDATE resources set checkedout_to_id = $2 where checkedout_to_id IS NULL AND name = $1", [params.resource_name, message.user], function(err, result) {
 				done();
 				if (err) {
-				console.error(err);
-				return;
+				  console.error(err);
+				    return;
 				} else {
-				// didn''t get a lock.
-				if (result.rows.length == 0) {
-				client.query('SELECT * FROM resources where name = $1', [params.resource_name], function(err, result) {
-					done();
-					if (err) {
-					bot.reply(message, 'Error finding resource ' + err);
-					console.error(err);
-					return;
-					} else {
-					if (result.rows.length == 0) {
-					bot.reply(message, "I couldn't find resource " + params.resource_name);
-					return;
-					}
-					}
+				  // Check to see if we updated
+				  if (result.rows.length == 0) {
+				    client.query('SELECT * FROM resources where name = $1', [params.resource_name], function(err, result) {
+					  done();
+					  if (err) {
+					    bot.reply(message, 'Error finding resource ' + err);
+					    console.error(err);
+					    return;
+					  } else {
+					    if (result.rows.length == 0) {
+					      bot.reply(message, "I couldn't find resource " + params.resource_name);
+					     return;
+			            } else {
+				          bot.reply(message, "Resource " + params.resource_name + " is already locked!!");
+			            }
+					  }
 					});	
-				} else {
+				  } else {
 					bot.reply(message, "Resource " + params.resource_name + " is checkout out  to you");
+				  }
 				}
-				});
-			} else {
-				bot.reply(message, "Resource " + params.resource_name + " is locked!!");
-			}
+              }
+	);
 	});
 }
+
 module.exports = {
   lockResource: lockResource
 }
